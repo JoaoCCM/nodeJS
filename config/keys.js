833c06dbf3426cc68866ddc88e56fc8a9eb110b2
JoaoCCM <joaocarlosc.m@hotmail.com>
2019-11-01 11:19:05 -0300

    criado

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://JC:6319525jc@cluster0-dvt97.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://user:password@cluster0-dvt97.mongodb.net/test?retryWrites=true&w=majority"
 };
